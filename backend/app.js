c2d4e48da0aa91db5161b63d43d0a7db24d2e770
telix <telegonicaxx@live.com>
2021-06-22 22:57:27 -0700

    initial checkpoint

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-   res.sendfile('index.html');
+		   res.sendFile('index.html');
+		
+});
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+		console.log('A user connected');
+
+		//Whenever someone disconnects this piece of code executed
+		socket.on('disconnect', function () {
+				console.log('A user disconnected');
+		});
 });
 
 http.listen(3000, function() {
-   console.log('listening on *:3000');
+		console.log('listening on *:3000');
 });
